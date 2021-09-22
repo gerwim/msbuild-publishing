f50cc2926faa8645cb8f58e1c335b5c882951e3a
Gerwim Feiken <info@gerwim.nl>
2021-09-22 21:11:06 +0200

    Updated build script

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 @ECHO OFF
 
 IF [%1] == [] (
-    SET PACKAGE_VERSION=0.0.0
+    ECHO Please run this script with the version parameter, e.g. "build.bat 1.0.0"
+    EXIT /b 1
 ) else (
     SET PACKAGE_VERSION=%1
 )
 
-powershell -command "& { Import-Module '%~dp0\build\psake\psake.psd1'; Invoke-psake '%~dp0\build\default.ps1' -taskList Test,Pack -parameters @{packageVersion='%PACKAGE_VERSION%'} }"
+powershell -ExecutionPolicy Bypass -command "& { Import-Module '%~dp0\build\psake\psake.psd1'; Invoke-psake '%~dp0\build\default.ps1' -taskList Test,Pack -parameters @{packageVersion='%PACKAGE_VERSION%'} }"
